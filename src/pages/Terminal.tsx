43e3d875b2465a92b836ca30b2329f22258d7dc7
Hans <ruinb0w@qq.com>
2025-08-03 20:04:32 +0800

    style: position terminal container absolutely

--- src/pages/Terminal.tsx
+++ src/pages/Terminal.tsx
@@ -3,7 +3,7 @@ import TermCom from "../components/TermCom.tsx"
 
 export default function TerminalPage() {
   return (
-    <div>
+    <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
       <TermCom />
     </div>
   )
